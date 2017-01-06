f7173afb14b2b2043f021beb6f9ca3b5eb0be648
glouhaichi <ghassen.louhaichi@gmail.com>
2017-01-06 13:57:31 +0100

    adding more optimized processing

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
 'use strict';
 
 $(document).ready(function () {
-
     var getColor = function (i, j) {
         return (i*j % 7 < i*j % 5 ? 'red' : 'yellow');
     };
-    var container = $('body'), pixelName = '', pixel = null, length = 400;
+    var container = $('<div class="container"></div>'), pixel = null, length = 2000;
     console.log('START');
     for (var i = 0; i < length; i++) {
+        console.log(i);
         for (var j = 0; j < length; j++) {
             var greyLevel = Mandelbrot.iterate(new Complex(Mandelbrot.getCoordinate(i, length), Mandelbrot.getCoordinate(j, length))).toString(16);
-            pixelName = 'p-' + i + '-' + j;
-            container.append('<div class="pixel ' + pixelName + '"></div>');
-            $('.' + pixelName).css({
-                'top': j,
-                'left': i,
-                'background-color': '#' + greyLevel + greyLevel + greyLevel
-            });
+            container.append('<div class="pixel" style="top:' + j + 'px; left:' + i + 'px; background-color:#' + greyLevel + greyLevel + greyLevel + ';"></div>');
         }
     }
+    $('body').append(container);
     console.log('END');
-
 });
